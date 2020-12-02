5b3ef06778e5e54fbad86e318176caddd060994d
David Tanzer <david@davidtanzer.net>
2020-12-02 02:07:45 +0100

    add placeholder component that shows current placeholder
    The placeholder component shows the current placeholder. This component
    is being updated when the user starts a new game.

--- src/app/render-hint/render-hint.component.spec.ts
+++ src/app/render-hint/render-hint.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { GameRulesService } from '../game-rules.service';
 
 import { RenderHintComponent } from './render-hint.component';
 
 describe('RenderHintComponent', () => {
   let component: RenderHintComponent;
   let fixture: ComponentFixture<RenderHintComponent>;
+  let gameRulesService: GameRulesService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +19,31 @@ describe('RenderHintComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RenderHintComponent);
     component = fixture.componentInstance;
+    gameRulesService = fixture.debugElement.injector.get(GameRulesService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('renders a hint with the correct length', (done) => {
+    gameRulesService.startNewGame('test');
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(fixture.debugElement.queryAll(By.css('.letter')).length).toBe(4);
+      done();
+    })
+  });
+
+  it('shows the correct placeholder character in each letter', (done) => {
+    gameRulesService.startNewGame('test');
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.debugElement.queryAll(By.css('.letter')).forEach(c => {
+        expect(c.nativeElement.textContent).toEqual('_');
+      })
+      done();
+    })
   });
 });
--- src/app/render-hint/render-hint.component.ts
+++ src/app/render-hint/render-hint.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { GameRulesService } from '../game-rules.service';
 
 @Component({
   selector: 'app-render-hint',
   templateUrl: './render-hint.component.html',
   styleUrls: ['./render-hint.component.css']
 })
-export class RenderHintComponent implements OnInit {
+export class RenderHintComponent implements OnInit, OnChanges {
+  placeholder: string[] = ['*', '*', ]
 
-  constructor() { }
+  constructor(public gameRulesService: GameRulesService) { }
 
   ngOnInit(): void {
+    this.placeholder = this.gameRulesService.placeholder()
   }
 
+  ngOnChanges(): void {
+    this.placeholder = this.gameRulesService.placeholder()
+  }
 }
